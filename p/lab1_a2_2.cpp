ad297506d732e4faaaed87ac56ede6744ec58402
Bobur <bobur.muhsimbaev@gmail.com>
2024-09-04 16:40:50 +0500

    upd

---
+++ p/lab1_a2_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+
+    cout << "Enter the first number...\n";
+    cin >> a;
+    cout << "Enter the second number...\n";
+    cin >> b;
+
+    cout << "result of a + b = " << a + b << endl;
+    
+
+
+    return 0;
+}
