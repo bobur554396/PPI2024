042ccc3fbdb5c169cc3af3741357237c8f9fd366
Bobur <bobur.muhsimbaev@gmail.com>
2024-11-19 14:48:32 +0500

    Lib: algorithm

---
+++ week12/G2/4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+/*
+1) 2, 2, 3, 4, 4, 5
+2) 2, 3, 4, 5, 4, 5
+               B
+*/
+
+
+int main(){
+    // - [ ] unique (begin, end)
+    int a[] = {2, 2, 3, 4, 4, 5};
+    vector<int> v(a, a + 6);
+
+    vector<int>::iterator bound = unique(v.begin(), v.end());
+    vector<int>::iterator begin = v.begin();
+    
+    while(begin != bound){
+        cout << *begin << " ";
+        begin++;
+    }
+
+
+    return 0;
+}
